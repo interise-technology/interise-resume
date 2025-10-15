4137b32ecf244ab866e4fa81244b40c42f5478ce
Yunus Tuzun <yunus.tuzun@interise.com.tr>
2025-10-15 15:15:03 +0300

    Added build folder.

---
+++ build/assets/js/a7456010.ea41f68e.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkinterise_resume=globalThis.webpackChunkinterise_resume||[]).push([[1235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
