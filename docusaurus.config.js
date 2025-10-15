db47133e6a024334bde5a58eb31cec16f63dbee9
Yunus Tuzun <yunus.tuzun@interise.com.tr>
2025-10-15 15:48:24 +0300

    Added build folders.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'http://resume.interise.space',
+  url: 'https://resume.interise.space',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/interise-resume/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
